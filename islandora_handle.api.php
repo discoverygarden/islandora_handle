2ea305433060d0f9addf0b09578e46d2330cfe47
Jordan Dukart <jordan@discoverygarden.ca>
2013-05-17 15:08:54 -0300

    Add API doc.

---
+++ islandora_handle.api.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Hooks provided by Islandora Handle.
+ */
+
+/**
+ * Retrieve module supplied XSL files for appending Handles to datastreams..
+ *
+ * @return array
+ *   Returns an associative array where the key is the URI to the file on the
+ *   file system and the value is the name of the file.
+ */
+function hook_islandora_handle_get_xsl_files() {
+}
